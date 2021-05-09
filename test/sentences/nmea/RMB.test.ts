fed95b80f5063e0c73aefe91962be5282893a9c6
Ricardo Boss <contact@ricardoboss.de>
2021-05-09 03:55:33 +0200

    Added RMA and RMB sentences

---
+++ test/sentences/nmea/RMB.test.ts
@@ -0,0 +1,9 @@
+import {describe} from "mocha";
+import {Decoder} from "../../../src";
+import {expect} from "chai";
+
+describe('Sentence', function () {
+	describe('RMB', function () {
+		// TODO: write tests for RMB
+	});
+});
